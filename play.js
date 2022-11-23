6d1a6b5f45d29025dac379d2860abf2ae69a4221
open-meadow <rohan.varughese@protonmail.com>
2022-11-23 22:44:02 +0000

    Implement setupInput and handleUserInput

--- play.js
+++ play.js
@@ -23,3 +23,24 @@ const connect = require('./client');
 
 console.log("Connecting...");
 connect();
+
+const handleUserInput = function(input) {
+  console.log(("Byeeeeee"));
+  console.log(input);
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+setupInput();
