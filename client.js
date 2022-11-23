1edb35b97232e2c62d86c51549241873c06a8a67
open-meadow <rohan.varughese@protonmail.com>
2022-11-23 20:09:19 +0000

    Play with moving the snek

--- client.js
+++ client.js
@@ -13,9 +13,53 @@ const connect = () => {
   conn.on("connect", () => {
     // console.log(connect);
     console.log("Connection Established");
-    conn.write("Name: yay")
+    // conn.write("Name: yay");
+    // setTimeout(() => {
+    //   conn.write("Move: right")
+    // }, 500);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 500);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 1000);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 1500);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 2000);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 2500);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 3000);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 3500);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 4000);
+
+    // setTimeout(() => {
+    //   conn.write("Move: left");
+    // }, 1000);
+
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 1500);
+
   })
-  
+
   conn.on("data", (messageFromServer) => {
     conn.write("me is snek")
     // code that does something when the connection is first established
